a9a01e5df2a0615e13d1093ad133ed052f62e7cc
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-02-16 22:36:04 +0700

    edit reviewdao

--- OnlineShop/src/main/java/com/shop/dao/ReviewDAO.java
+++ OnlineShop/src/main/java/com/shop/dao/ReviewDAO.java
@@ -8,5 +8,5 @@ public interface ReviewDAO  {
 	List<Review> getReviews(int productid);
 	boolean addReview(Review review);
 	boolean editReview(Review review);
-	boolean deleteReview(int reviewid);
+	boolean deleteReview(int reviewID);
 } 
