d1eaf0261ebe8ad186417afef0cd20e77389c3b3
Synv4 <Synv4@192.168.248.1>
2022-02-17 00:04:55 +0700

    reviewController

--- OnlineShop/src/main/java/com/shop/controller/ReviewController.java
+++ OnlineShop/src/main/java/com/shop/controller/ReviewController.java
@@ -1,11 +1,17 @@
 package com.shop.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.shop.dao.ReviewDAO;
+import com.shop.dao.impl.ReviewDAOImpl;
+import com.shop.model.Review;
 
 /**
  * Servlet implementation class ReviewController
@@ -21,13 +27,16 @@ public class ReviewController extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ReviewDAOImpl rvImp= new ReviewDAOImpl();
+		int pid = Integer.parseInt(request.getParameter("pid"));
+		List<Review> lsrv= rvImp.getReviews(pid);
+		request.getRequestDispatcher(getServletInfo()).forward(request, response);
 	}
 
 	/**
@@ -37,5 +46,5 @@ public class ReviewController extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
+	
 }
