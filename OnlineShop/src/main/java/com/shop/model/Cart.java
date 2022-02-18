db85cf778fc504f898154e852315d3cad0c71aca
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-02-18 22:59:47 +0700

    update cart

--- OnlineShop/src/main/java/com/shop/model/Cart.java
+++ OnlineShop/src/main/java/com/shop/model/Cart.java
@@ -17,5 +17,93 @@ public class Cart {
 	private int quantity;
 	private double price;
 	private int numberPage;
+	public Cart() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getProductID() {
+		return productID;
+	}
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getIssuingCompany() {
+		return issuingCompany;
+	}
+	public void setIssuingCompany(String issuingCompany) {
+		this.issuingCompany = issuingCompany;
+	}
+	public LocalDateTime getPublicationDate() {
+		return publicationDate;
+	}
+	public void setPublicationDate(LocalDateTime publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+	public int getCoverType() {
+		return coverType;
+	}
+	public void setCoverType(int coverType) {
+		this.coverType = coverType;
+	}
+	public String getPublishingCompany() {
+		return publishingCompany;
+	}
+	public void setPublishingCompany(String publishingCompany) {
+		this.publishingCompany = publishingCompany;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getNumberPage() {
+		return numberPage;
+	}
+	public void setNumberPage(int numberPage) {
+		this.numberPage = numberPage;
+	}
+	
 
 }
