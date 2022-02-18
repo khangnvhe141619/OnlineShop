76d8f27134f8f1fa8748f544314d9e84c5e30ebc
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-02-18 22:59:06 +0700

    create model cart

---
+++ OnlineShop/src/main/java/com/shop/model/Cart.java
@@ -0,0 +1,21 @@
+package com.shop.model;
+
+import java.time.LocalDateTime;
+
+public class Cart {
+	private int productID;
+	private int categoryId;
+	private String categoryName;
+	private String productName;
+	private String image;
+	private String description;
+	private LocalDateTime createdDate;
+	private String issuingCompany;
+	private LocalDateTime publicationDate;
+	private int coverType;
+	private String publishingCompany;
+	private int quantity;
+	private double price;
+	private int numberPage;
+
+}
