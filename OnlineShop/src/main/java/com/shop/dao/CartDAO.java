e1860ffb94428df0ab14b329785871f0e2fe2e03
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-02-18 23:12:26 +0700

    update cartdao

--- OnlineShop/src/main/java/com/shop/dao/CartDAO.java
+++ OnlineShop/src/main/java/com/shop/dao/CartDAO.java
@@ -3,25 +3,16 @@ package com.shop.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.shop.model.Account;
 import com.shop.model.Product;
 
 public interface CartDAO {
-	//get product by id
-	public Product getProductById(String id) throws SQLException;
-
-	//add product to db
-	public boolean insertProduct(Product product) throws SQLException;
-
-	//update product to db
-	public boolean updateProduct(Product product) throws SQLException;
-
-	//delete product which have id="id" from db
-	public boolean deleteProduct(String id) throws SQLException;
-
-	//get total of  product
-	public List<Product> getListAllProduct(int row) throws SQLException;
+Account getLogin(String userName, String password) throws SQLException;
 	
-	//
-	public int countTotalProduct() throws SQLException;
+	boolean getCheckUsername(String userName) throws SQLException;
+	
+	boolean getCheckEmail(String email) throws SQLException;
+	
+	boolean getInsertAccount(Account account) throws SQLException;
 
 }
