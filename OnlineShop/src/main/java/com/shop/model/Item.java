13ee7103dc4cfceed2b52ac64fdca66dd5c62cef
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-02-25 09:48:45 +0700

    update Item model

--- OnlineShop/src/main/java/com/shop/model/Item.java
+++ OnlineShop/src/main/java/com/shop/model/Item.java
@@ -7,6 +7,30 @@ public class Item {
 	public Item() {
 		// TODO Auto-generated constructor stub
 	}
+	public Item(Product product, int quantity, double price) {
+		super();
+		this.product = product;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	
 	
 }
