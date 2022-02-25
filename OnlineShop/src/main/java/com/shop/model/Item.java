0599980e3bdfddc155a395cd347cbb4d419d1d79
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-02-25 09:47:55 +0700

    cm item model

---
+++ OnlineShop/src/main/java/com/shop/model/Item.java
@@ -0,0 +1,12 @@
+package com.shop.model;
+
+public class Item {
+	private Product product;
+	private int quantity;
+	private double price;
+	public Item() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
