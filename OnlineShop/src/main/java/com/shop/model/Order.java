4b0116389abbd29e69265d2045cb1d5f12f8cf84
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-02-27 09:50:23 +0700

    update order

--- OnlineShop/src/main/java/com/shop/model/Order.java
+++ OnlineShop/src/main/java/com/shop/model/Order.java
@@ -11,30 +11,13 @@ public class Order {
 	private LocalDate EstimateDelivery;
 	private Double total;
 	private int statusId;
-	private int productId;
-	private int quantity;
+
 	public Order() {
 		// TODO Auto-generated constructor stub
 	}
-	
-
-
-	public Order(int orderId, int accountId, Double total, int productId, int quantity) {
-		super();
-		this.orderId = orderId;
-		this.accountId = accountId;
-		this.total = total;
-		this.productId = productId;
-		this.quantity = quantity;
-	}
-
-
-
-
-
 
 	public Order(int orderId, int shipId, int accountId, LocalDate orderDate, LocalDate estimateDelivery, Double total,
-			int statusId, int productId, int quantity) {
+			int statusId) {
 		super();
 		this.orderId = orderId;
 		this.shipId = shipId;
@@ -43,27 +26,28 @@ public class Order {
 		EstimateDelivery = estimateDelivery;
 		this.total = total;
 		this.statusId = statusId;
-		this.productId = productId;
-		this.quantity = quantity;
 	}
-
-
 
 	public int getOrderId() {
 		return orderId;
 	}
+
 	public void setOrderId(int orderId) {
 		this.orderId = orderId;
 	}
+
 	public int getShipId() {
 		return shipId;
 	}
+
 	public void setShipId(int shipId) {
 		this.shipId = shipId;
 	}
+
 	public int getAccountId() {
 		return accountId;
 	}
+
 	public void setAccountId(int accountId) {
 		this.accountId = accountId;
 	}
@@ -72,58 +56,41 @@ public class Order {
 		return orderDate;
 	}
 
-
-
 	public void setOrderDate(LocalDate orderDate) {
 		this.orderDate = orderDate;
 	}
-
-
 
 	public LocalDate getEstimateDelivery() {
 		return EstimateDelivery;
 	}
 
-
-
 	public void setEstimateDelivery(LocalDate estimateDelivery) {
 		EstimateDelivery = estimateDelivery;
 	}
 
-
-
 	public Double getTotal() {
 		return total;
 	}
+
 	public void setTotal(Double total) {
 		this.total = total;
 	}
+
 	public int getStatusId() {
 		return statusId;
 	}
+
 	public void setStatusId(int statusId) {
 		this.statusId = statusId;
 	}
-	public int getProductId() {
-		return productId;
-	}
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-
 
 	@Override
 	public String toString() {
 		return "Order [orderId=" + orderId + ", shipId=" + shipId + ", accountId=" + accountId + ", orderDate="
 				+ orderDate + ", EstimateDelivery=" + EstimateDelivery + ", total=" + total + ", statusId=" + statusId
-				+ ", productId=" + productId + ", quantity=" + quantity + "]";
+				+ "]";
 	}
+	
+
 
 }
