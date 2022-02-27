35d2953e6ed85263ab877513d95d593dd6dc2bd5
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-02-27 09:43:05 +0700

    update cartdao

--- OnlineShop/src/main/java/com/shop/dao/CartDAO.java
+++ OnlineShop/src/main/java/com/shop/dao/CartDAO.java
@@ -4,15 +4,10 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.shop.model.Account;
+import com.shop.model.Cart;
 import com.shop.model.Product;
 
 public interface CartDAO {
-Account getLogin(String userName, String password) throws SQLException;
-	
-	boolean getCheckUsername(String userName) throws SQLException;
-	
-	boolean getCheckEmail(String email) throws SQLException;
-	
-	boolean getInsertAccount(Account account) throws SQLException;
+	public void addCard(Account acc, Cart cart);
 
 }
