81cb7311ffc17d63bb8085257b4d0b9e02578f6c
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-02-27 09:34:19 +0700

    cm orderdetail

---
+++ OnlineShop/src/main/java/com/shop/model/Orderdetail.java
@@ -0,0 +1,35 @@
+package com.shop.model;
+
+public class Orderdetail {
+	private int orderId;
+	private int productId;
+	private int quantity;
+	public Orderdetail() {
+		// TODO Auto-generated constructor stub
+	}
+	public Orderdetail(int orderId, int productId, int quantity) {
+		super();
+		this.orderId = orderId;
+		this.productId = productId;
+		this.quantity = quantity;
+	}
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+}
