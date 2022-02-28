7487930fc3accbd0cc61566d6b520cf656b695bd
Nguyen Sy Dong <80672342+sydong207@users.noreply.github.com>
2022-02-28 14:34:24 +0700

    update AccountDAOImpl.java

--- OnlineShop/src/main/java/com/shop/dao/impl/AccountDAOImpl.java
+++ OnlineShop/src/main/java/com/shop/dao/impl/AccountDAOImpl.java
@@ -80,7 +80,7 @@ public class AccountDAOImpl implements AccountDAO {
 			pre = con.prepareStatement(SQLCommand.insertAccount);
 			pre.setString(1, account.getUsername());
 			pre.setString(2, account.getPassword());
-			pre.setString(3, account.getcreatedDate());
+			pre.setString(3, account.getCreatedDate());
 			check = pre.executeUpdate() == 1;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
