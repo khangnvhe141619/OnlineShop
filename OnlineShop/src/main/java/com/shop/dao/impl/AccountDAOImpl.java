ade64c2073b048f50deeb38a3728e552dd212612
Nguyen Sy Dong <80672342+sydong207@users.noreply.github.com>
2022-02-28 14:35:36 +0700

    Update AccountDAOImpl.java

--- OnlineShop/src/main/java/com/shop/dao/impl/AccountDAOImpl.java
+++ OnlineShop/src/main/java/com/shop/dao/impl/AccountDAOImpl.java
@@ -107,9 +107,8 @@ public class AccountDAOImpl implements AccountDAO {
 			pre = con.prepareStatement(SQLCommand.listAccount);
 			rs = pre.executeQuery();
 			while (rs.next()) {
-				accounts.add(new Account(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
-						rs.getString(5), rs.getInt(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getInt(10),
-						rs.getInt(11), rs.getString(12)));
+				accounts.add(new Account(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), 
+						rs.getString(6), rs.getString(7), rs.getString(8), rs.getInt(9), rs.getInt(10), rs.getString(11)));
 			}
 
 		} finally {
