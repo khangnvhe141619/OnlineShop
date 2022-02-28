1691c1397645a131b00cec692073222003ce3248
anhldhe141746 <anhldhe141746@fpt.edu.vn>
2022-03-01 01:38:02 +0700

    zzz

--- OnlineShop/src/main/java/com/shop/model/Discount.java
+++ OnlineShop/src/main/java/com/shop/model/Discount.java
@@ -10,5 +10,19 @@ package com.shop.model;
  * @author leduc
  */
 public class Discount {
+    private int discountId;
+    private String discountDesc;
+    private int discountPercent;
+    private int quantity;
+
+    public Discount() {
+    }
+
+    public Discount(int discountId, String discountDesc, int discountPercent, int quantity) {
+        this.discountId = discountId;
+        this.discountDesc = discountDesc;
+        this.discountPercent = discountPercent;
+        this.quantity = quantity;
+    }
     
 }
