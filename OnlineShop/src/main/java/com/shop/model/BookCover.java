b6bbb7066ab6a8b581b2da6e05567f50ff807165
anhldhe141746 <anhldhe141746@fpt.edu.vn>
2022-02-28 18:56:11 +0700

    Dont understand this

--- OnlineShop/src/main/java/com/shop/model/BookCover.java
+++ OnlineShop/src/main/java/com/shop/model/BookCover.java
@@ -10,5 +10,36 @@ package com.shop.model;
  * @author leduc
  */
 public class BookCover {
+    private int bookCoverId;
+    private String bookCoverName;
+
+    public BookCover() {
+    }
+
+    public BookCover(int bookCoverId, String bookCoverName) {
+        this.bookCoverId = bookCoverId;
+        this.bookCoverName = bookCoverName;
+    }
+
+    public int getBookCoverId() {
+        return bookCoverId;
+    }
+
+    public void setBookCoverId(int bookCoverId) {
+        this.bookCoverId = bookCoverId;
+    }
+
+    public String getBookCoverName() {
+        return bookCoverName;
+    }
+
+    public void setBookCoverName(String bookCoverName) {
+        this.bookCoverName = bookCoverName;
+    }
+
+    @Override
+    public String toString() {
+        return "BookCover{" + "bookCoverId=" + bookCoverId + ", bookCoverName=" + bookCoverName + '}';
+    }
     
 }
