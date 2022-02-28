6d49aa3297b33a8eab8bedda56ff14c1172ab96b
khangnvhe141619 <97073195+khangnvhe141619@users.noreply.github.com>
2022-02-28 23:30:54 +0700

    Delete OnlineShop/OnlineShop directory

--- OnlineShop/OnlineShop/src/main/java/com/shop/dao/ContactDAO.java
+++
@@ -1,9 +0,0 @@
-package com.shop.dao;
-
-import java.sql.SQLException;
-
-import com.shop.model.Contact;
-
-public interface ContactDAO {
-	public Contact getInformation() throws SQLException;
-}
--- OnlineShop/OnlineShop/src/main/java/com/shop/dao/SubscriberDAO.java
+++
@@ -1,9 +0,0 @@
-package com.shop.dao;
-
-import java.sql.SQLException;
-
-import com.shop.model.Subscriber;
-
-public interface SubscriberDAO {
-	public boolean insertSubscriber(Subscriber subscriber) throws SQLException;
-}
