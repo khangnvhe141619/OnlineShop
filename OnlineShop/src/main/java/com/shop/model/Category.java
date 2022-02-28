d6f72877184d0c0fd2605f44be37507f632434f5
Ha Anh Dung <dunghahe140640@fpt.edu.vn>
2022-03-01 00:00:50 +0700

    update category model

--- OnlineShop/src/main/java/com/shop/model/Category.java
+++ OnlineShop/src/main/java/com/shop/model/Category.java
@@ -4,6 +4,35 @@ public class Category {
 	private int categoryID;
 	private String categoryName;
 
-	
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(int categoryID, String categoryName) {
+		super();
+		this.categoryID = categoryID;
+		this.categoryName = categoryName;
+	}
+
+	public int getCategoryID() {
+		return categoryID;
+	}
+
+	public void setCategoryID(int categoryID) {
+		this.categoryID = categoryID;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [categoryID=" + categoryID + ", categoryName=" + categoryName + "]";
+	}
 
 }
