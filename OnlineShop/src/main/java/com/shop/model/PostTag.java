6b846528480f018ba92ffa8aa1e84547a4afc370
anhldhe141746 <anhldhe141746@fpt.edu.vn>
2022-03-01 01:43:20 +0700

    zzz

--- OnlineShop/src/main/java/com/shop/model/PostTag.java
+++ OnlineShop/src/main/java/com/shop/model/PostTag.java
@@ -20,5 +20,26 @@ public class PostTag {
         this.postId = postId;
         this.tagId = tagId;
     }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(int tagId) {
+        this.tagId = tagId;
+    }
+
+    @Override
+    public String toString() {
+        return "PostTag{" + "postId=" + postId + ", tagId=" + tagId + '}';
+    }
     
 }
