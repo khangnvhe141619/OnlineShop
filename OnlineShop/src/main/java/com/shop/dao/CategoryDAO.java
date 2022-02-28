2ac091050092dee32e1b832312e81f621a745e15
Ha Anh Dung <dunghahe140640@fpt.edu.vn>
2022-03-01 00:01:52 +0700

    update CategoryDao

--- OnlineShop/src/main/java/com/shop/dao/CategoryDAO.java
+++ OnlineShop/src/main/java/com/shop/dao/CategoryDAO.java
@@ -7,16 +7,6 @@ import com.shop.model.Category;
 import com.shop.model.Product;
 
 public interface CategoryDAO {
-	public List<Category> getListAllCategory() throws SQLException;
 	
-	public Category getCategoryByID(String categoryId) throws SQLException;
-	
-	public List<Product> getListProductByCategory(String categoryId) throws SQLException;
-	
-	public boolean insertCategory(Category category) throws SQLException;
-	
-	public boolean updateCategory(Category category) throws SQLException;
-	
-	public boolean deleteCategory(String categoryId) throws SQLException;
 	
 }
