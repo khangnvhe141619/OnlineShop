56e2b632f68b2ee8b927badea4d8544e93896d5f
anhldhe141746 <anhldhe141746@fpt.edu.vn>
2022-02-28 17:00:01 +0700

    create model

---
+++ OnlineShop/src/main/java/com/shop/model/Address.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shop.model;
+
+/**
+ *
+ * @author leduc
+ */
+public class Address {
+    private int addressId;
+    private String address;
+
+    public Address() {
+    }
+
+    public Address(int addressId, String address) {
+        this.addressId = addressId;
+        this.address = address;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+}
