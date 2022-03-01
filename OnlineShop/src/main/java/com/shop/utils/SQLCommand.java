8ddb621c0c4ef49730d941c4e258416d36525449
Nguyen Sy Dong <80672342+sydong207@users.noreply.github.com>
2022-03-01 09:19:36 +0700

    update sqlCommand

--- OnlineShop/src/main/java/com/shop/utils/SQLCommand.java
+++ OnlineShop/src/main/java/com/shop/utils/SQLCommand.java
@@ -2,15 +2,18 @@ package com.shop.utils;
 
 public class SQLCommand {
 
-	public static final String login = "SELECT * FROM Account WHERE (Username LIKE ? OR Email LIKE ?) AND Password LIKE ?";
+	public static final String LOGIN = "SELECT * FROM Account WHERE (Username LIKE ? OR Email LIKE ?) AND Password LIKE ?";
 	
-	public static final String checkUsername = "SELECT * FROM Account WHERE Username like ?";
+	public static final String GET_ACCOUNT_FROM_ACCOUNT = "SELECT * FROM Account WHERE Username like ?";
 	
-	public static final String checkEmail = "SELECT * FROM Account WHERE Email LIKE ?";
+	public static final String GET_ACCOUNT_FROM_EMAIL = "SELECT * FROM Account WHERE Email LIKE ?";
 	
-	public static final String insertAccount = "INSERT INTO Account(Username, Password, CreatedDate, Role, Active)\r\n"
+	public static final String INSERT_ACCOUNT = "INSERT INTO Account(Username, Password, CreatedDate, Role, Active)\r\n"
 			+ "VALUES(?,?,?,1,1)";
 	
-	public static final String listAccount = "SELECT * FROM Account";
+	public static final String LIST_ACCOUNT = "SELECT * FROM Account";
+	
+	public static final String UPDATE_ACCOUNT = "UPDATE Account SET Username = ?, FullName = ?, Email = ?, PhoneNumber = '?\r\n"
+			+ "WHERE AccountID = ?\r\n";
 
 }
