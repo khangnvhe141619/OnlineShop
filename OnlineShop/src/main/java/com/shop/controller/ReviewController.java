58a0ec14d64ff38cfa5f388a16d92be1c30e54ee
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-03-03 23:58:40 +0700

    Cm reviewCOntroller

--- OnlineShop/src/main/java/com/shop/controller/ReviewController.java
+++ OnlineShop/src/main/java/com/shop/controller/ReviewController.java
@@ -1,6 +1,8 @@
 package com.shop.controller;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -8,17 +10,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.shop.dao.ReviewDAO;
 import com.shop.dao.impl.ReviewDAOImpl;
+import com.shop.model.Account;
 import com.shop.model.Review;
 
 /**
  * Servlet implementation class ReviewController
  */
-@WebServlet("/ReviewController")
+@WebServlet("/review")
 public class ReviewController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final Date date = null;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -33,17 +38,40 @@ public class ReviewController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ReviewDAOImpl rvImp= new ReviewDAOImpl();
-		int pid = Integer.parseInt(request.getParameter("pid"));
-		List<Review> lsrv= rvImp.getReviews(pid);
-		
+		try {
+			HttpSession ss = request.getSession(true);
+			int accid= (int) ss.getAttribute("account");
+			
+			 String pid = request.getParameter("pid");
+			 int id =Integer.parseInt(pid);
+		     String ratet = request.getParameter("rating");
+		     int rate = Integer.parseInt(ratet);
+		     String content = request.getParameter("content");
+		     LocalDate localDate = java.time.LocalDate.now();
+		     String date = localDate.toString();
+		   
+		     
+		     System.out.println(id);
+		  
+		     System.out.println(content);
+		     System.out.println(rate);
+		     System.out.println(date);
+		     ReviewDAOImpl rv= new ReviewDAOImpl();
+		     Review review = new Review();
+		   
+					rv.addReview(id, accid, content, rate, date);
+					request.setAttribute("id", id);
+					request.getRequestDispatcher("/list").forward(request, response);
+				
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 	
