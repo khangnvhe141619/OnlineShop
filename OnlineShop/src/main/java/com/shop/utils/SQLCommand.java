10985d642c0707d05070cd9ba0cd7af7b28c485d
Nguyen Sy Dong <80672342+sydong207@users.noreply.github.com>
2022-03-03 20:24:26 +0700

    SQLCommand

--- OnlineShop/src/main/java/com/shop/utils/SQLCommand.java
+++ OnlineShop/src/main/java/com/shop/utils/SQLCommand.java
@@ -17,6 +17,12 @@ public class SQLCommand {
 	public static final String INSERT_ACCOUNT = "INSERT INTO Account(Username, Password, CreatedDate, Role, Active)\r\n"
 			+ "VALUES(?,?,?,1,1)";
 	
+	public static final String INSERT_Order = "INSERT INTO [Order] VALUES(?, ?, ?, ?, ?)";
+	
+	public static final String SELECT_Order_MAX = "select max(OrderID)[mx] from [Order]";
+	
+	public static final String INSERT_OrderDetail = "INSERT INTO OrderDetail VALUES (?,?,?)";
+	
 	public static final String LIST_ACCOUNT = "SELECT * FROM Account";
 	
 	public static final String UPDATE_ACCOUNT = "UPDATE Account SET Username = ?, FullName = ?, Email = ?, PhoneNumber = ?, Avatar = ?\r\n"
