99af6260aa8405493587c9cb697566a8ae72de48
Nguyen Sy Dong <80672342+sydong207@users.noreply.github.com>
2022-03-04 13:18:05 +0700

    SQLCommand

--- OnlineShop/src/main/java/com/shop/utils/SQLCommand.java
+++ OnlineShop/src/main/java/com/shop/utils/SQLCommand.java
@@ -28,11 +28,25 @@ public class SQLCommand {
 	public static final String UPDATE_ACCOUNT = "UPDATE Account SET Username = ?, FullName = ?, Email = ?, PhoneNumber = ?, Avatar = ?\r\n"
 			+ "WHERE AccountID = ?\r\n";
 
-	public static final String GET_LIST_ORDER = "SELECT O.Total, OS.Description, P.Image, P.ProductName, P.Price, OD.Quantity\r\n"
+	public static final String GET_LIST_ORDER = "SELECT O.Total, OS.Description, P.Image, P.ProductName, P.Price, OD.Quantity, P.ProductID\r\n"
 			+ "FROM [Order] O JOIN OrderDetail OD \r\n"
 			+ "ON O.OrderID = OD.OrderID JOIN OrderStatus OS \r\n"
 			+ "ON OS.ID = O.StatusId JOIN Product P\r\n"
 			+ "ON P.ProductId = OD.ProductId\r\n"
 			+ "WHERE O.AccountId = ?";
+	
+	public static final String GET_LIST_ORDER_BY_PENDING = "SELECT O.Total, OS.Description, P.Image, P.ProductName, P.Price, OD.Quantity, P.ProductID\r\n"
+			+ "FROM [Order] O JOIN OrderDetail OD \r\n"
+			+ "ON O.OrderID = OD.OrderID JOIN OrderStatus OS \r\n"
+			+ "ON OS.ID = O.StatusId JOIN Product P\r\n"
+			+ "ON P.ProductId = OD.ProductId\r\n"
+			+ "WHERE O.AccountId = ? AND OS.Description like 'Pending'";
+	
+	public static final String GET_LIST_ORDER_BY_COMPLETED = "SELECT O.Total, OS.Description, P.Image, P.ProductName, P.Price, OD.Quantity, P.ProductID\r\n"
+			+ "FROM [Order] O JOIN OrderDetail OD \r\n"
+			+ "ON O.OrderID = OD.OrderID JOIN OrderStatus OS \r\n"
+			+ "ON OS.ID = O.StatusId JOIN Product P\r\n"
+			+ "ON P.ProductId = OD.ProductId\r\n"
+			+ "WHERE O.AccountId = ? AND OS.Description like 'Completed'";
 	
 }
