ee051c830464a9d0410d1c5373074454b1039c35
synvhe141716 <synvhe141716@fpt.edu.vn>
2022-03-04 09:38:57 +0700

    uPDATE reviewDAOimpl

--- OnlineShop/src/main/java/com/shop/dao/impl/ReviewDAOImpl.java
+++ OnlineShop/src/main/java/com/shop/dao/impl/ReviewDAOImpl.java
@@ -4,9 +4,12 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.shop.dao.ReviewDAO;
 import com.shop.model.Account;
@@ -18,6 +21,28 @@ public  class ReviewDAOImpl implements ReviewDAO {
 	private Connection conn = null;
 	private PreparedStatement ps = null;
 	private ResultSet rs = null;
+	
+	public boolean isBought(int productid, int accid) {
+        try {
+            String sql = "select * from OrderDetail as od, [Order] as o \n"
+            		+ " where o.orderid = od.orderid and productid = ? and o.AccountId = ?";
+            conn = DBConnection.getInstance().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, productid);
+            ps.setInt(2, accid);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ReviewDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return false;
+    }
 
 	public List<Review> getReviews(int productid) {
 		List<Review> list = new ArrayList<Review>();
@@ -72,14 +97,20 @@ public  class ReviewDAOImpl implements ReviewDAO {
 //		for (Review review : ls) {
 //			System.out.println(review.toString());
 //		}
-		LocalDate localDate = java.time.LocalDate.now();
-	     String date = localDate.toString();
-	   
-		Boolean kq= rv.addReview(1, 1, "aaaa", 2, date);
-			if(kq==true) {
-		System.out.println("sccc");
-			}else {
-			System.err.println("failes");
+//		LocalDate localDate = java.time.LocalDate.now();
+//	     String date = localDate.toString();
+//	   
+//		Boolean kq= rv.addReview(1, 1, "aaaa", 2, date);
+//			if(kq==true) {
+//		System.out.println("sccc");
+//			}else {
+//			System.err.println("failes");
+//		}
+		boolean kq = rv.isBought(1, 2);
+		if(kq==true) {
+			System.out.println("ok");
+		}else {
+			System.out.println("fa");
 		}
 	}
 
