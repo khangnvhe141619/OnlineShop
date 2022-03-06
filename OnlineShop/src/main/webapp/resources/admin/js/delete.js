88ddb264b4cbae511efa032f28356e21f2956a6a
Nguyen Sy Dong <80672342+sydong207@users.noreply.github.com>
2022-03-06 11:26:14 +0700

    delete.js

--- OnlineShop/src/main/webapp/resources/admin/js/delete.js
+++ OnlineShop/src/main/webapp/resources/admin/js/delete.js
@@ -1,10 +1,3 @@
-function showMes(id) {
-	var option = confirm('Are you sure to delete' + id);
-	if (option === true) {
-		window.location.href = 'DeleteController?id=' + id;
-	}
-}
-
 function showCheck() {
 	var departmentName = document.getElementById("departmentName").value;
 	if (departmentName.length > 50) {
