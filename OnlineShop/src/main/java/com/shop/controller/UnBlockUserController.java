4d05441af78287c16bb8ee7bdd60815ce8cb99c2
Nguyen Sy Dong <80672342+sydong207@users.noreply.github.com>
2022-03-06 20:20:29 +0700

    UnBlock

---
+++ OnlineShop/src/main/java/com/shop/controller/UnBlockUserController.java
@@ -0,0 +1,52 @@
+package com.shop.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.shop.dao.AccountDAO;
+import com.shop.dao.impl.AccountDAOImpl;
+
+/**
+ * Servlet implementation class UnBlockUserController
+ */
+@WebServlet("/unBlockUserController")
+public class UnBlockUserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UnBlockUserController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int accountID = Integer.parseInt(request.getParameter("accountID"));
+		try {
+			AccountDAO dao = new AccountDAOImpl();
+			dao.getUnBlockAccount(accountID);
+			response.sendRedirect("listUserBlockController");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
