8e204d309354528535caeb53526e66a238d0b18d
S2WINNERS2\Synvhe141716 <synvhe141716@fpt.edu.vn>
2022-03-08 00:32:38 +0700

    finsh deletecategory

---
+++ OnlineShop/src/main/java/com/shop/controller/DeleteCateController.java
@@ -0,0 +1,62 @@
+package com.shop.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.shop.dao.impl.CategoryDAOImpl;
+import com.shop.dao.impl.ProductDAOImpl;
+
+/**
+ * Servlet implementation class DeleteCateController
+ */
+@WebServlet("/DeleteCateController")
+public class DeleteCateController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteCateController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CategoryDAOImpl pd = new CategoryDAOImpl();		
+		String pageIn=request.getParameter("index");
+		int index= Integer.parseInt(pageIn);
+		boolean kq;
+		try {
+			kq = pd.deleteCategory(index);
+			if(kq==true) {
+				request.setAttribute("mess", "Delete successfull");
+				request.getRequestDispatcher("/CategoryController?index=1").forward(request, response);
+			}else {
+				request.setAttribute("mess", "Delete False");
+				request.getRequestDispatcher("/CategoryController?index=1").forward(request, response);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
