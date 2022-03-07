b11e6140df584a9d385e3d875670e9e4509b702f
S2WINNERS2\Synvhe141716 <synvhe141716@fpt.edu.vn>
2022-03-08 00:28:37 +0700

    Update details product

--- OnlineShop/src/main/java/com/shop/controller/DetailsProductController.java
+++ OnlineShop/src/main/java/com/shop/controller/DetailsProductController.java
@@ -61,8 +61,7 @@ public class DetailsProductController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
