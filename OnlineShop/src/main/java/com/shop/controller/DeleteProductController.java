2850ccb7e9be48fc3e17bb05b85503ed65281e53
S2WINNERS2\Synvhe141716 <synvhe141716@fpt.edu.vn>
2022-03-08 00:32:57 +0700

    finsh delete product

---
+++ OnlineShop/src/main/java/com/shop/controller/DeleteProductController.java
@@ -0,0 +1,62 @@
+package com.shop.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.shop.dao.impl.ProductDAOImpl;
+import com.shop.model.Product;
+
+/**
+ * Servlet implementation class DeleteController
+ */
+@WebServlet("/DeleteController")
+public class DeleteProductController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteProductController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProductDAOImpl pd = new ProductDAOImpl();		
+		String pageIn=request.getParameter("index");
+		int index= Integer.parseInt(pageIn);
+		boolean kq;
+		try {
+			kq = pd.deleteProduct(index);
+			if(kq==true) {
+				request.setAttribute("mess", "Delete successfull");
+				request.getRequestDispatcher("/ListProdtuct?index=1").forward(request, response);
+			}else {
+				request.setAttribute("mess", "Delete False");
+				request.getRequestDispatcher("/ListProdtuct?index=1").forward(request, response);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
