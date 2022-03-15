9fb6d7a813bdb98e12c176bb3457f220616d1b41
Ha Anh Dung <dunghahe140640@fpt.edu.vn>
2022-03-15 07:31:17 +0700

    commit

--- OnlineShop/src/main/java/com/shop/dao/AccountDAO.java
+++ OnlineShop/src/main/java/com/shop/dao/AccountDAO.java
@@ -6,7 +6,7 @@ import com.shop.model.Account;
 
 
 public interface AccountDAO {
-	Account getLogin(String userName, String password) throws SQLException;
+	Account getLogin(String userName, String password) throws 
 	
 	boolean getCheckUsername(String userName) throws SQLException;
 	
