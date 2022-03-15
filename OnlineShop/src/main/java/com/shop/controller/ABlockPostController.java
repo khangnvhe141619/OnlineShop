311b774ed706f1720695ae1d708bf4e24da7eef6
Ha Anh Dung <dunghahe140640@fpt.edu.vn>
2022-03-15 07:38:34 +0700

    update controller

--- OnlineShop/src/main/java/com/shop/controller/ABlockPostController.java
+++ OnlineShop/src/main/java/com/shop/controller/ABlockPostController.java
@@ -51,7 +51,7 @@ public class ABlockPostController extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
